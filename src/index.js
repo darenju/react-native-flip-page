cd377add9a936fe2563b260342cdfdfd81d7056a
Julien Fradin <julien@frad.in>
2018-08-15 01:40:21 +0200

    Fix duplicate declaration of styles

--- src/index.js
+++ src/index.js
@@ -375,48 +375,6 @@ class FlipPagePage extends React.PureComponent {
   }
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  page: {
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    width: '100%',
-    height: '100%',
-  },
-  half: {
-    position: 'absolute',
-    left: 0,
-    top: 0,
-    overflow: 'hidden',
-  },
-  verticalHalf: {
-    height: '50%',
-    width: '100%',
-  },
-  horizontalHalf: {
-    height: '100%',
-    width: '50%',
-  },
-  under: {
-    backgroundColor: '#000',
-  },
-  verticalFirstHalf: {
-    top: 0,
-  },
-  verticalSecondHalf: {
-    top: '50%',
-  },
-  horizontalFirstHalf: {
-    left: 0,
-  },
-  horizontalSecondHalf: {
-    left: '50%',
-  },
-});
-
 export {
   FlipPage as default,
   FlipPagePage
